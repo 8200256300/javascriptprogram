014c53c26513ce0731c836ddf2b15b9b85d8c425
Mahil <khanparamahil2710.com>
2026-01-29 13:29:11 +0530

    Object file added

--- object.js
+++ object.js
@@ -38,3 +38,74 @@ console.log(student.address.city);
 console.log(Object.keys(student));
 console.log(Object.values(student));
 console.log(Object.entries(student));
+
+//
+const user = {
+    name: "Mahil Patel",
+    role: "Admin",
+    designation: "developer",
+    age: 20,
+    address: {
+        city: "keshod",
+        zip: 362220,
+    }
+}
+
+const {name, age, role} = user;
+console.log(name);
+console.log(age);
+console.log(role);
+
+const {name: profilename, age: profileage} = user;
+console.log(profilename);
+console.log(profileage);
+
+const array = [1,2,3];
+const moreElements = [...array,4,5,6];
+console.log(moreElements);
+
+const OriginalArray = [1,2,3,4,5];
+const copiedArray = [...OriginalArray];
+console.log(copiedArray);
+
+const array1 = [10,20,30];
+const array2 = [40,50,60];
+const mergedArray = [...array1, ...array2];
+console.log(mergedArray);
+
+//spread operator in object destructuring
+
+const user1 = {
+    username: "MahilPatel",
+    city: "keshod",
+}
+console.log("Original user1:", user1);
+
+const newObj = {
+    ...user1,
+    role: "Admin",
+};
+console.log("New Object:", newObj);
+
+
+//copying object
+const user2 = {
+    username: "MahilKhanpara",
+    city: "keshod",
+}
+const user3 = {...user2};
+console.log("Copied Object:", user3);
+
+//merging object
+const obj1 = {
+    name: "Mahil",
+    age: 20,
+}
+const obj2 = {
+    city: "keshod",
+    role: "Developer",
+    age: 25,
+}
+const Obj3 = {...obj1, ...obj2};
+console.log("Merged Object:", Obj3);
+
